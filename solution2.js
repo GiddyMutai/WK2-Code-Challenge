cf310af842167868943ad6ae9912cf8209764da6
Gideon Mutai <giddymutai13@gmail.com>
2024-04-11 00:13:20 +0300

    updates changes

--- solution2.js
+++ solution2.js
@@ -3,12 +3,20 @@
 let noArray = []; // initializes an empty array to store the numbers
 
 function numberRange(num1, num2) {
-  let i = num1 - 1; // sets the initialized number to one less the first number for it to be included
-  while (i < num2) {
-    i++;
-    noArray.push(i); // appends the number to the array
+  if (num1<num2){ // checks if num1 is greater than num2
+    let i = num1 - 1; // sets the initialized number to one less the first number for it to be included
+    while (i < num2) {
+      i++;
+      noArray.push(i); // appends the number to the array
+    }
+  } else if (num1>num2){
+    let i = num1 + 1; // sets the initialized number to one less the first number for it to be included
+    while (i > num2) {
+      i--;
+      noArray.push(i); // appends the number to the array
+    }
   }
-  return noArray; // returns the resultant array
+  return noArray // returns the outputted array
 }
 
-console.log(numberRange(-4, 7)); // An example input
+console.log(numberRange(-7, -17)); // An example input
